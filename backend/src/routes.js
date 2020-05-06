f243d4b6643a695426423a2887693eb44459561a
Lissa Ferreira <lissadeveloper@gmail.com>
2020-05-06 19:41:38 -0400

    first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const {celebrate, Segments, Joi} = require('celebrate')
+
+const usersController = require('./controllers/usersController')
+
+const routes = express.Router()
+
+
+routes.get('/users', usersController.index)
+
+routes.post('/users', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        apps: Joi.string()
+    })
+}) ,usersController.create)
+
+routes.put('/users', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        apps: Joi.string()
+    })
+}) ,usersController.change)
+
+module.exports = routes
