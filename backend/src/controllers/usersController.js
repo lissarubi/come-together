75fa2c8a4b21038235a5eee98618308378cb0d2b
Ederson Ferreira <edersondeveloper@gmail.com>
2020-05-09 16:02:39 -0400

    prettier

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,31 +1,39 @@
-const connection = require('../database/connection')
-const crypto = require('crypto')
+const connection = require('../database/connection');
+const crypto = require('crypto');
 
 module.exports = {
-    async index(request, response){
-        const users = await connection('users').select('*')
-        return response.json(users)
-    },
+  async index(request, response) {
+    const users = await connection('users').select('*');
+    return response.json(users);
+  },
 
-    async create(request, response){
-        const {name, apps} = request.body
-        
-        const id = crypto.randomBytes(8).toString('HEX')
-    
-        await connection('users').insert({
-            id,
-            name,
-            apps
+  async create(request, response) {
+    const { name, apps } = request.body;
+
+    const id = crypto.randomBytes(8).toString('HEX');
+
+    await connection('users').insert({
+      id,
+      name,
+      password,
+      apps,
+    });
+
+    return response.json({ id });
+  },
+
+  async change(request, response) {
+    const { username, password, apps } = request.body;
+    try {
+      await connection('users')
+        .where({
+          username: username,
+          password: password,
         })
-    
-        return response.json({ id })
-    },
-    
-    async change( request, response ){
-        const {name, apps} = request.body
-
-        await connection('users').where('name', '=', name).update({ apps: apps })
-        
-        return response.status(204).send()
+        .update({ apps: apps });
+      return response.status(204).send();
+    } catch (err) {
+      console.log(err);
     }
-}
+  },
+};
